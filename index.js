e78d5c37828510a620f82c85e9dcd1cd3dc79ff0
Helge Hofstad <helge.hofstad@gmail.com>
2016-11-29 00:59:55 +0100

    more parser options

--- index.js
+++ index.js
@@ -2,31 +2,49 @@
 
 const express = require('express');
 const http = require('http');
-
 const xml2js = require('xml2js');
-const parser = new xml2js.Parser();
 
+const parserOptions = {
+  explicitArray: false, 
+  async: true,
+  mergeAttrs: true
+};
+const parser = new xml2js.Parser(parserOptions);
 const app = express();
 
-app.get('/', (req, res) => {
-  let url = req.query.url || '';
+app.get('/feed/:url', (req, res) => {
+
+  let url = req.params.url || req.query.feed;
+  let jsonp = !!(req.query.callback);
   let xmlString = '';
-  http.get(url, function(response) {
+
+  if (!url)
+    res.status(400).end();
+
+  let onSuccess = function(response) {
     response.on('data', function(chunk) {
       xmlString += chunk.toString();
     });
     response.on('end', function(r) {
       parser.parseString(xmlString, function(err, result) {
         if (err) {
-          res.status(500).end();
+          res.status(404).end();
           return;
         }
-        res.json(result);
+        if (jsonp)
+          res.jsonp(result.rss);
+        else
+          res.json(result.rss);
       });
     });
-  }, function(err) {
+  };
+
+  let error = function(err) {
     res.status(500).end();
-  });
+  };
+
+  http.get(url, onSuccess, error);
+
 });
 
 app.listen(80);
